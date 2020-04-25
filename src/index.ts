cd7f588fd8220a8f2745e00f58d2eae068d80566
poblouin <poblouin@protonmail.com>
2020-04-25 14:11:01 +1000

    split into multiple files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
@@ -6,3 +7,16 @@ import { HomebridgeSpotifyPlatform } from './platform';
 export = (api: API) => {
   api.registerPlatform(PLATFORM_NAME, HomebridgeSpotifyPlatform);
 };
+=======
+import type { API } from 'homebridge';
+
+import { PLATFORM_NAME } from './settings';
+import { ExampleHomebridgePlatform } from './platform'; 
+
+/**
+ * This method registers the platform with Homebridge
+ */
+export = (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+}
+>>>>>>> 7d40dc0 (split into multiple files)
--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
 export const PLATFORM_NAME = 'HomebridgeSpotify';
 
 export const PLUGIN_NAME = '@poblouin/homebridge-spotify';
+=======
+/**
+ * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
+ */
+export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
+
+/**
+ * This must match the name of your plugin as defined the package.json
+ */
+export const PLUGIN_NAME = 'homebridge-plugin-name';
+>>>>>>> 7d40dc0 (split into multiple files)
