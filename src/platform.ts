03c4e7ac3c8a10dcfaf14bc46cb94f9b95c79236
poblouin <poblouin@protonmail.com>
2021-05-11 11:22:35 -0400

    basic setup

--- src/platform.ts
+++ src/platform.ts
@@ -8,7 +8,7 @@ import { ExamplePlatformAccessory } from './platformAccessory';
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class HomebridgeSpotifyPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
