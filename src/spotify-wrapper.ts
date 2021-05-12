5536a31332c8a456079b5dace0b194f905688552
poblouin <poblouin@protonmail.com>
2021-05-11 21:56:34 -0400

    spotify auth flow + persist tokens

--- src/spotify-wrapper.ts
+++ src/spotify-wrapper.ts
@@ -1,17 +1,22 @@
-import { Logger, PlatformConfig } from 'homebridge';
+import fs from 'fs';
+
+import { API, Logger, PlatformConfig } from 'homebridge';
 import SpotifyWebApi from 'spotify-web-api-node';
 import {
   DEFAULT_SPOTIFY_CALLBACK,
   SPOTIFY_AUTH_ERROR,
   SPOTIFY_MISSING_CONFIGURATION_ERROR,
+  SPOTIFY_REFRESH_TOKEN_ERROR,
 } from './constants';
 
 export class SpotifyWrapper {
   private spotifyApi: SpotifyWebApi;
+  private persistPath: string;
 
   constructor(
     public readonly log: Logger,
     public readonly config: PlatformConfig,
+    public readonly api: API,
   ) {
     if (
       !config.spotifyClientId ||
@@ -24,16 +29,40 @@ export class SpotifyWrapper {
       throw new Error(SPOTIFY_MISSING_CONFIGURATION_ERROR);
     }
 
+    this.persistPath = `${api.user.persistPath()}/.homebridge-spotify`;
+
     this.spotifyApi = new SpotifyWebApi({
       clientId: config.spotifyClientId,
       clientSecret: config.spotifyClientSecret,
       redirectUri: DEFAULT_SPOTIFY_CALLBACK,
     });
 
-    this.login(config.spotifyAuthCode);
+    this.authenticate(config.spotifyAuthCode);
+    this.persistTokens();
+    this.log.debug('Spotify auth success');
   }
 
-  async login(authCode) {
+  async authenticate(authCode) {
+    await this.fetchTokensFromStorage();
+    if (this.spotifyApi.getAccessToken()) {
+      return;
+    }
+
+    await this.authWithCodeGrant(authCode);
+    if (this.spotifyApi.getAccessToken()) {
+      return;
+    }
+
+    this.log.error(
+      `We could not fetch the Spotify tokens nor authenticate using the code grant flow.
+        Please redo the manual login step, provide the new auth code in the config then try again.`,
+    );
+    throw new Error(SPOTIFY_AUTH_ERROR);
+  }
+
+  async authWithCodeGrant(authCode) {
+    this.log.debug('Attempting the code grant authorization flow');
+
     try {
       const data = await this.spotifyApi.authorizationCodeGrant(authCode);
 
@@ -44,8 +73,64 @@ export class SpotifyWrapper {
       this.spotifyApi.setAccessToken(data.body['access_token']);
       this.spotifyApi.setRefreshToken(data.body['refresh_token']);
     } catch (err) {
-      this.log.error('Could not authorize Spotify: \n\n', err);
+      this.log.error('Could not authorize Spotify:\n\n', err);
       throw new Error(SPOTIFY_AUTH_ERROR);
+    }
+  }
+
+  async fetchTokensFromStorage() {
+    this.log.debug('Attempting to fetch tokens saved in the storage');
+
+    let tokens;
+    try {
+      tokens = JSON.parse(
+        fs.readFileSync(this.persistPath, { encoding: 'utf-8' }),
+      );
+    } catch {
+      // File not created yet
+      return;
+    }
+
+    if (!tokens.accessToken || ! tokens.refreshToken) {
+      return;
+    }
+
+    this.spotifyApi.setAccessToken(tokens.accessToken);
+    this.spotifyApi.setRefreshToken(tokens.refreshToken);
+    this.log.debug(
+      'Successfully fetched the tokens from storage, going to refresh the access token',
+    );
+
+    try {
+      await this.refreshToken();
+    } catch {
+      // Reset the creds since they are wrong, we will try the code auth grant instead.
+      this.spotifyApi.resetCredentials();
+    }
+  }
+
+  persistTokens() {
+    const writeData = JSON.stringify({
+      accessToken: this.spotifyApi.getAccessToken(),
+      refreshToken: this.spotifyApi.getRefreshToken(),
+    });
+
+    try {
+      fs.writeFileSync(this.persistPath, writeData);
+    } catch (err) {
+      this.log.warn('Failed to persist tokens, the plugin will not be able to authenticate when homebridge restarts:\n\n', err);
+    }
+  }
+
+  async refreshToken() {
+    try {
+      const data = await this.spotifyApi.refreshAccessToken();
+      this.log.debug('The access token has been refreshed!');
+
+      this.spotifyApi.setAccessToken(data.body['access_token']);
+    } catch (err) {
+      this.log.debug('Could not refresh access token:\n\n', err);
+      throw new Error(SPOTIFY_REFRESH_TOKEN_ERROR);
     }
   }
 }
