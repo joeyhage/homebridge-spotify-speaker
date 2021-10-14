89d5171303ef0c7673e34ec338d885be10b5fa93
poblouin <poblouin@protonmail.com>
2021-10-14 16:06:31 -0400

    fix typo

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,6 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { HomebridgeSpotifyPlatform } from './platform';
 
 /**
  * Platform Accessory
@@ -20,7 +20,7 @@ export class ExamplePlatformAccessory {
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: HomebridgeSpotifyPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
