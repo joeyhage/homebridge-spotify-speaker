24bc65c4612077bde8f409f70cc5e7e41e580e65
poblouin <poblouin@protonmail.com>
2022-01-28 08:31:08 -0500

    use lightbulb instead of speaker

--- src/spotify-speaker-accessory.ts
+++ src/spotify-speaker-accessory.ts
@@ -1,14 +1,15 @@
-import { Service, PlatformAccessory, Logger } from 'homebridge';
+import { Service, PlatformAccessory, Logger, Categories } from 'homebridge';
 
 import { HomebridgeSpotifyPlatform } from './platform';
 import { HomebridgeSpotifyDevice } from './types';
 
-export class SpotifySpeakerAccessory {
+export class SpotifyFakeSpeakerAccessory {
   private static DEFAULT_POLL_INTERVAL_MS = 20 * 1000;
   private service: Service;
-  private activeState: number;
-  private isMuted: boolean;
+  private activeState: boolean;
   private currentVolume: number;
+
+  public static CATEGORY = Categories.LIGHTBULB;
 
   constructor(
     private readonly platform: HomebridgeSpotifyPlatform,
@@ -18,29 +19,24 @@ export class SpotifySpeakerAccessory {
   ) {
     this.service =
       this.accessory.getService(
-        this.platform.Service.Speaker,
+        this.platform.Service.Lightbulb,
       ) ||
       this.accessory.addService(
-        this.platform.Service.Speaker,
+        this.platform.Service.Lightbulb,
       );
 
     this.service.updateCharacteristic(this.platform.Characteristic.Name, this.device.deviceName);
 
-    this.service.getCharacteristic(this.platform.Characteristic.Mute)
-      .onGet(this.handleMuteGet.bind(this))
-      .onSet(this.handleMuteSet.bind(this));
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onGet(this.handleOnGet.bind(this))
+      .onSet(this.handleOnSet.bind(this));
 
-    this.service.getCharacteristic(this.platform.Characteristic.Active)
-      .onGet(this.handleActiveGet.bind(this))
-      .onSet(this.handleActiveSet.bind(this));
+    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
+      .onGet(this.handleBrightnessGet.bind(this))
+      .onSet(this.handleBrightnessSet.bind(this));
 
-    this.service.getCharacteristic(this.platform.Characteristic.Volume)
-      .onGet(this.handleVolumeGet.bind(this))
-      .onSet(this.handleVolumeSet.bind(this));
-
-    this.activeState = this.platform.Characteristic.Active.ACTIVE;
+    this.activeState = false;
     this.currentVolume = 0;
-    this.isMuted = false;
 
     this.setInitialState();
 
@@ -51,74 +47,54 @@ export class SpotifySpeakerAccessory {
       const oldVolume = this.currentVolume;
 
       if (state.statusCode === 200) {
-        this.activeState = this.getActiveState(state.body.is_playing);
+        this.activeState = state.body.is_playing;
         this.currentVolume = state.body.device.volume_percent;
       } else if (state.statusCode === 204) {
-        this.activeState = this.platform.Characteristic.Active.INACTIVE;
+        this.activeState = false;
         this.currentVolume = 0;
       }
 
       if (oldActiveState !== this.activeState) {
-        this.service.getCharacteristic(this.platform.Characteristic.Active).updateValue(this.activeState);
+        this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(this.activeState);
       }
       if (oldVolume !== this.currentVolume) {
-        this.service.updateCharacteristic(this.platform.Characteristic.Volume, this.currentVolume);
+        this.service.updateCharacteristic(this.platform.Characteristic.Brightness, this.currentVolume);
       }
-    }, SpotifySpeakerAccessory.DEFAULT_POLL_INTERVAL_MS);
+    }, SpotifyFakeSpeakerAccessory.DEFAULT_POLL_INTERVAL_MS);
 
   }
 
-  handleActiveGet(): number {
+  handleOnGet(): boolean {
     this.log.debug('Triggered GET Active:', this.activeState);
     return this.activeState;
   }
 
-  async handleActiveSet(value): Promise<void> {
+  async handleOnSet(value): Promise<void> {
     this.log.debug('Triggered SET Active:', value);
-
-    switch (value) {
-      case this.platform.Characteristic.Active.ACTIVE:
-        await this.platform.spotifyApiWrapper.play(this.device.spotifyDeviceId, this.device.spotifyPlaylistId);
-        break;
-      case this.platform.Characteristic.Active.INACTIVE:
-        await this.platform.spotifyApiWrapper.pause(this.device.spotifyDeviceId);
-        break;
-
-      default:
-        this.log.error('Unknown target media state:', value);
-        break;
+    if (value === this.activeState) {
+      return;
     }
+
+    value
+      ? this.platform.spotifyApiWrapper.play(this.device.spotifyDeviceId, this.device.spotifyPlaylistId)
+      : this.platform.spotifyApiWrapper.pause(this.device.spotifyDeviceId);
 
     this.activeState = value;
   }
 
-  async handleVolumeGet() {
-    this.log.debug('Triggered GET CurrentMediaState:', this.currentVolume);
+  async handleBrightnessGet() {
+    this.log.debug('Get volume', this.currentVolume);
     return this.currentVolume;
   }
 
-  async handleVolumeSet(value): Promise<void> {
-    this.log.debug('Set volume');
+  async handleBrightnessSet(value): Promise<void> {
+    this.log.debug('Set volume:', value);
+    if (value === this.currentVolume) {
+      return;
+    }
+
     await this.platform.spotifyApiWrapper.setVolume(value, this.device.spotifyDeviceId);
     this.currentVolume = value;
-  }
-
-  handleMuteGet() {
-    this.log.debug('Triggered GET Mute:', this.isMuted);
-    return this.isMuted;
-  }
-
-  async handleMuteSet(): Promise<void> {
-    this.log.debug('Set mute');
-    const volume = this.isMuted ? 50 : 0;
-    await this.platform.spotifyApiWrapper.setVolume(volume, this.device.spotifyDeviceId);
-    this.isMuted = !this.isMuted;
-  }
-
-  private getActiveState(isPlaying: boolean): number {
-    return isPlaying
-      ? this.platform.Characteristic.Active.ACTIVE
-      : this.platform.Characteristic.Active.INACTIVE;
   }
 
   private async setInitialState(): Promise<void> {
@@ -126,15 +102,14 @@ export class SpotifySpeakerAccessory {
     const state = await this.platform.spotifyApiWrapper.getPlaybackState();
 
     if (state.statusCode === 200) {
-      this.activeState = this.getActiveState(state.body.is_playing);
+      this.activeState = state.body.is_playing;
       this.currentVolume = state.body.device.volume_percent;
     } else if (state.statusCode === 204) {
-      this.activeState = this.platform.Characteristic.Active.INACTIVE;
+      this.activeState = false;
       this.currentVolume = 0;
     }
 
-    this.service.getCharacteristic(this.platform.Characteristic.Active).updateValue(this.activeState);
-    this.service.getCharacteristic(this.platform.Characteristic.Volume).updateValue(this.currentVolume);
-    this.service.getCharacteristic(this.platform.Characteristic.Mute).updateValue(this.isMuted);
+    this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(this.activeState);
+    this.service.getCharacteristic(this.platform.Characteristic.Brightness).updateValue(this.currentVolume);
   }
 }
