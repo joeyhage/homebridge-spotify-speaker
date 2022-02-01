b33194419fd826a57b9d271b97758b4ea27917a8
poblouin <poblouin@protonmail.com>
2022-02-01 08:05:02 -0500

    Prep for release

--- src/platform.ts
+++ src/platform.ts
@@ -9,10 +9,15 @@ import {
 } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-// import { SpotifySmartSpeakerAccessory } from './spotify-smart-speaker-accessory';
+import { SpotifySmartSpeakerAccessory } from './spotify-smart-speaker-accessory';
 import { SpotifyFakeSpeakerAccessory } from './spotify-speaker-accessory';
 import { SpotifyApiWrapper } from './spotify-api-wrapper';
+import { SPOTIFY_MISSING_CONFIGURATION_ERROR } from './constants';
 
+const DEVICE_CLASS_CONFIG_MAP = {
+  speaker: SpotifyFakeSpeakerAccessory,
+  smartSpeaker: SpotifySmartSpeakerAccessory,
+};
 export class HomebridgeSpotifySpeakerPlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
@@ -45,32 +50,36 @@ export class HomebridgeSpotifySpeakerPlatform implements DynamicPlatformPlugin {
 
   discoverDevices() {
     for (const device of this.config.devices) {
+      const deviceClass = this.getDeviceConstructor(device.deviceType);
       const uuid = this.api.hap.uuid.generate(device.spotifyDeviceId);
       const existingAccessory = this.accessories.find((accessory) => accessory.UUID === uuid);
 
       if (existingAccessory) {
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-        // Update the playlist ID if it changed
         existingAccessory.context.device = device;
         this.api.updatePlatformAccessories([existingAccessory]);
 
-        // new SpotifySmartSpeakerAccessory(this, existingAccessory, device, this.log);
-        new SpotifyFakeSpeakerAccessory(this, existingAccessory, device, this.log);
+        new deviceClass(this, existingAccessory, device, this.log);
       } else {
         this.log.info('Adding new accessory:', device.deviceName);
 
-        const accessory = new this.api.platformAccessory(device.deviceName, uuid, SpotifyFakeSpeakerAccessory.CATEGORY);
-
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
+        const accessory = new this.api.platformAccessory(device.deviceName, uuid, deviceClass.CATEGORY);
         accessory.context.device = device;
 
-        // new SpotifySmartSpeakerAccessory(this, accessory, device, this.log);
-        new SpotifyFakeSpeakerAccessory(this, accessory, device, this.log);
+        new deviceClass(this, accessory, device, this.log);
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
       }
     }
+  }
+
+  private getDeviceConstructor(deviceType) {
+    if (!deviceType) {
+      this.log.error('It is missing the `deviceType` in the configuration.');
+      throw new Error(SPOTIFY_MISSING_CONFIGURATION_ERROR);
+    }
+
+    return DEVICE_CLASS_CONFIG_MAP[deviceType];
   }
 
   private async logAvailableSpotifyDevices() {
