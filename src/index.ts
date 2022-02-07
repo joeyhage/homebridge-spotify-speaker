9f847c2238feb109614e8583359156767ba5e8b6
poblouin <poblouin@protonmail.com>
2022-02-06 21:46:04 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings';
 import { HomebridgeSpotifySpeakerPlatform } from './platform';
 
+
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, HomebridgeSpotifySpeakerPlatform);
+  api.
+  registerPlatform(PLATFORM_NAME, HomebridgeSpotifySpeakerPlatform);
 };
